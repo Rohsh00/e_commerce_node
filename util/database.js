0bbb0efd1a37de528f9be9c5f62596c5ae0d9d9b
Rohit Sharma <rohsh00@gmail.com>
2023-08-20 01:58:54 +0530

    first commit

---
+++ util/database.js
@@ -0,0 +1,30 @@
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
+
+let _db;
+
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    "mongodb+srv://rohit:" +
+      encodeURIComponent("rohit@123") + // URL encode the password
+      "@cluster0.kjphf3z.mongodb.net/shop?retryWrites=true&w=majority"
+  )
+    .then((client) => {
+      console.log("Connected!");
+      _db = client.db();
+      callback();
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+};
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw "No database found";
+};
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
